20127cb1a8c5e65cb4e0a226f5963ef043e7f5e3
pythoninma <athulrameshan17@gmail.com>
2022-08-30 18:28:14 +0530

    add new b

--- school/school_management/school_management/settings.py
+++ school/school_management/school_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9z8lvv1!$-y!nzb@p=c5)ov6*35q#t@9#8h!5df1$qs*hz!ho1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
